86481d58e14714649415bc50dc60bbafefdfb06c
Sam Sintz <samsintz@gmail.com>
2015-07-28 16:25:58 -0600

    added new upgrade: faster iv drip. added new message to appear after learning to code

--- game.js
+++ game.js
@@ -15,14 +15,17 @@ var strawRow = document.getElementById("strawRow");
 var wideStrawRow = document.getElementById("wideStrawRow");
 var IVRow = document.getElementById("IVRow");
 var learnToCodeRow = document.getElementById("learnToCodeRow");
+var fasterIVRow = document.getElementById("fasterIVRow");
 //buttons
 var strawUpgrade = document.getElementById("strawUpgrade");
 var wideStrawUpgrade = document.getElementById("wideStrawUpgrade");
 var IVUpgrade = document.getElementById("IVUpgrade");
 var learnToCodeUpgrade = document.getElementById("learnToCodeUpgrade");
+var fasterIVUpgrade = document.getElementById("fasterIVUpgrade");
 //other
 var moneyText = document.getElementById("money");
 var messageIV = document.getElementById("messageIV");
+var messageCode = document.getElementById("messageCode");
 
 var upgrades =
     {
@@ -45,6 +48,11 @@ var upgrades =
             upgraded: false,
             cost: 100,
             effect: 0.5
+        },
+        fasterIVUpgrade: {
+            upgraded: false,
+            cost: 150,
+            effect: 1
         }
     };
 
@@ -53,14 +61,17 @@ function init() {
     strawUpgrade.disabled = true;
     wideStrawUpgrade.disabled = true;
     IVUpgrade.disabled = true;
-    learnToCodeUpgrade.disabled = "true";
+    learnToCodeUpgrade.disabled = true;
+    fasterIVUpgrade.disabled = true;
     
     //rows
     learnToCodeRow.style.visibility= "hidden";
+    fasterIVRow.style.visibility = "hidden";
     
     //other
     moneyText.style.visibility = "hidden";
     messageIV.style.visibility = "hidden";
+    messageCode.style.visibility = "hidden";
     
     //start intervals and check for interface changes
     manageIntervals();
@@ -124,6 +135,21 @@ learnToCodeUpgrade.onclick = function() {
     learnToCodeRow.style.display = "none";
     
     upgrades.learnToCodeUpgrade.upgraded = true;
+
+    //show next upgrade
+    fasterIVRow.style.visibility = "visible";
+
+    //update messages
+    manageMessages();
+    manageIntervals();
+};
+
+fasterIVUpgrade.onclick = function () {
+    sips -= upgrades.fasterIVUpgrade.cost;
+    updateTotalSips();
+    fasterIVRow.style.display = "none";
+
+    upgrades.fasterIVUpgrade.upgraded = true;
     manageIntervals();
 };
 
@@ -153,6 +179,12 @@ function manageUpgradeTable() {
         learnToCodeUpgrade.disabled = true;   
     }
     
+    if(sips >= upgrades.fasterIVUpgrade.cost) {
+        fasterIVUpgrade.disabled = false;
+    } else {
+        fasterIVUpgrade.disabled = true;
+    }
+
 }
 
 function manageMessages() {
@@ -160,6 +192,9 @@ function manageMessages() {
         messageIV.style.visibility = "visible";   
     }
     
+    if(upgrades.learnToCodeUpgrade.upgraded) {
+        messageCode.style.visibility = "visible";
+    }
 }
 
 
@@ -167,13 +202,17 @@ function manageIntervals() {
     setInterval(function() {
         manageUpgradeTable();
         manageMessages();
-    },100);
+    }, 100);
     
     if(upgrades.IVUpgrade.upgraded) {
         clearInterval(autoSip);
         
         autoSip = setInterval(function() {
-            sips += upgrades.IVUpgrade.effect;
+            if(upgrades.fasterIVUpgrade.upgraded) {
+                sips += (upgrades.IVUpgrade.effect + upgrades.fasterIVUpgrade.effect);
+            } else {
+               sips += upgrades.IVUpgrade.effect;
+            }
             updateTotalSips();
         }, autoSipRate);
         
